5eee15d67442c711b2c1e200b0e70f9d1adb07c0
kratoon <ondrej@kratochvil.dev>
2024-06-27 22:43:15 +0200

    Fix components.md conflict with components.html

--- scripts/buildComponentsOverviewPage.js
+++ scripts/buildComponentsOverviewPage.js
@@ -2,13 +2,15 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const overviewDir = path.join(__dirname, '../subsites/component-library/docs/overview');
+const mdFile = path.join(overviewDir, 'components.md');
+const htmlFile = path.join(overviewDir, '_components.html');
 
-const template = fs.readFileSync(path.join(overviewDir, 'components.md'), 'utf-8');
-const html = fs.readFileSync(path.join(overviewDir, 'components.html'), 'utf-8');
+const template = fs.readFileSync(mdFile, 'utf-8');
+const html = fs.readFileSync(htmlFile, 'utf-8');
 
 const startSeparator = '<!-- libscript -->';
 const stopSeparator = '<!-- libscriptstop -->';
 const start = template.split(startSeparator)[0];
 const stop = template.split(stopSeparator)[1];
 
-fs.writeFileSync(path.join(overviewDir, 'components.md'), [start, startSeparator, '\n', html, stopSeparator, stop].join(''));
+fs.writeFileSync(mdFile, [start, startSeparator, '\n', html, stopSeparator, stop].join(''));
